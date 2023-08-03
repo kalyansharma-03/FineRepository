b14c2940a9e08918053e9d288340adebe946c157
Samiksha141 <samikshaphuyel6@gmail.com>
2023-08-03 14:16:13 +0545

    Fine collection interface service added

--- FineTable.Domain/Entities/EFineCollection.cs
+++ FineTable.Domain/Entities/EFineCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,13 @@ namespace FineTable.Domain.Entities
 {
     public class EFineCollection
     {
+        public int Id { get; set; }
+        public string MemberID { get; set; }
+        public int Amount { get; set; }
+        public int Days { get; set; }
+        public MemberTypes MemberType { get; set; }
+        public DateTime CreatedDate {get; set;}
+        public DateTime ReturnDate{get; set;}
+        
     }
 }
--- FineTable.Infrastructure/Service/FineCollectionService.cs
+++ FineTable.Infrastructure/Service/FineCollectionService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FineTable.Domain.Entities;
+using FineTable.Domain.Interface;
+using FineTable.Infrastructure.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace FineTable.Infrastructure.Service
 {
-    internal class FineCollectionService
+    public class FineCollectionService : IFineCollectionService
     {
+        private readonly IServiceFactory _serviceFactory;
+
+        public FineCollectionService(IServiceFactory serviceFactory)
+        {
+            try
+            { 
+                //var service = _serviceFactory.GetInstance<>
+            } catch (Exception ex) { }
+        }
+
+        public Task<bool> CreateFineCollection(EFineCollection eFineCollection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteFineCollection(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EFineCollection> GetFineCollectionById(EFineCollection eFineCollection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<EFineCollection>> GetFineCollections()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateFineCollection(EFineCollection eFineCollection)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
